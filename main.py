a694d895e30c96a19fc45aa018b1620bfc0dd184
po3rin <abctail30@gmail.com>
2024-06-05 03:47:44 +0900

    rm debug screenshot

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import feedparser
 import pandera.polars as pa
 import polars as pl
 from pandera.typing.polars import LazyFrame
+import requests
+from bs4 import BeautifulSoup
 
 
 COMPASS_RSS = "https://connpass.com/explore/ja.atom"
@@ -12,7 +14,6 @@ class EventSchema(pa.DataFrameModel):
     link: str
     summary: str
     published: str
-    recommend: bool
 
 
 class KeywordSchema(pa.DataFrameModel):
@@ -31,7 +32,6 @@ def get_event() -> LazyFrame[EventSchema]:
             "link": event["link"],
             "summary": event["summary"],
             "published": event["published"],
-            "recommend": False,
         }
         for event in feed["entries"]
     )
@@ -39,28 +39,66 @@ def get_event() -> LazyFrame[EventSchema]:
 
 
 @pa.check_types
-def load_keywords() -> LazyFrame[KeywordSchema]:
+def load_keywords_from_csv(csv_path: str) -> LazyFrame[KeywordSchema]:
     """
     Load keywords from the CSV file.
     """
-    return pl.scan_csv("keywords.csv")
+    return pl.scan_csv(csv_path)
 
 
 @pa.check_types
 def extract_events_include_keywords(events_lf: LazyFrame[EventSchema], keywords_lf: LazyFrame[KeywordSchema]) -> LazyFrame[EventSchema]:
+    """
+    Extract events that include keywords.
+    """
     keywords_list = keywords_lf.to_pandas()["key"].tolist()
-    events_lf["recommend"] = events_lf["summary"].str.contains("|".join(keywords_list))
-    return events_lf
+    return events_lf.filter(pl.col("summary").str.contains("|".join(keywords_list)))
 
 
-def main() -> None:
+def extract_event_participants_info(link: str) -> dict[str, dict[str, int]]:
+    """
+    Check if the event has a presentation.
+    """
+    response = requests.get(link, timeout=10, headers={'User-Agent': ''})
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    results = {}
+    paticipations = soup.select('.ptype')
+    for paticipation in paticipations:
+        name = paticipation.select('.ptype_name')[0].get_text()
+        amount_txt = paticipation.select('.participants > .amount')[0].get_text()
+        amout_txt = amount_txt.replace('人', '')
+        amount_numerator = int(amout_txt.split('/')[0])
+        amount_denominator = int(amout_txt.split('/')[1])
+        results[name] = {"numerator": amount_numerator, "denominator": amount_denominator}
+    return results
+
+
+def is_events_has_presentation(participants_info: dict[str, dict[str,int]]) -> bool:
+    """
+    Check if the event has a presentation.
+    """
+    keys = participants_info.keys()
+    for key in keys:
+        if "発表" in key or "プレゼン" in key or "LT" in key or "lightning talk" in key or "登壇" in key:
+            return True
+    return False
+
+
+def connpass_events(keywords_csv_path: str) -> None:
+    """
+    Main function to get connpass events.
+    """
     events_lf = get_event().collect()
-    keywords_lf = load_keywords().collect()
+    keywords_lf = load_keywords_from_csv(keywords_csv_path).collect()
     events_lf = extract_events_include_keywords(events_lf, keywords_lf)
 
-    print(events_lf)
-    print(keywords_lf)
+    links = events_lf.to_pandas()["link"].tolist()
+    for link in links:
+        participants_info = extract_event_participants_info(link)
+        has_presentation = is_events_has_presentation(participants_info)
+        print(f"{link}, {has_presentation}, {participants_info}")
 
 
 if __name__ == "__main__":
-    main()
+    connpass_events("keywords.csv")
